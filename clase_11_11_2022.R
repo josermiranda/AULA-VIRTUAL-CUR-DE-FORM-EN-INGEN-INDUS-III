e35a85a44cabc297ff45d0c7a9884d508a0ed46c
josermiranda <josermirandah@gmail.com>
2022-11-12 06:39:56 +0000

    ##analisis de variables numericas # Histogramas # normalidad ## Distribución empírica acumulada

--- clase_11_11_2022.R
+++ clase_11_11_2022.R
@@ -91,6 +91,91 @@ Tabla_frecuencias<-fdt(mtcars$disp,breaks="Sturges",start = min, end= max , by =
 Tabla_frecuencias
 
 
+## Distribución empírica acumulada
+
+#En estadística , una función de distribución empírica (comúnmente también llamada función de distribución acumulativa empírica, eCDF) es la función de distribución asociada con la medida empírica de una muestra . Esta función de distribución acumulativa es una función escalonada que salta 1 / n en cada uno de los n puntos de datos. Su valor en cualquier valor especificado de la variable medida es la fracción de observaciones de la variable medida que son menores o iguales al valor especificado.
+#La función de distribución empírica es una estimación de la función de distribución acumulada que generó los puntos en la muestra. Converge con probabilidad 1 a esa distribución subyacente, según el teorema de Glivenko-Cantelli . Existen varios resultados para cuantificar la tasa de convergencia de la función de distribución empírica con la función de distribución acumulativa subyacente.
+#En teoría de la probabilidad, una medida empírica es una medida aleatoria procedente de una realización particular de una secuencia (finita) de variables aleatorias
+
+#va observando la varicion acumulada a traves de la curva
+
+plot(mtcars$mpg, pnorm(mtcars$mpg, mean = mean(mtcars$mpg), sd = sd(mtcars$mpg)), ylab = "", lwd = 2, col = "red")
+
+#Añadimos la media    
+abline(v=mean(mtcars$mpg))
+
+#Funcion escalonada de distribucion de probabilidad
+
+plot(qnorm, pnorm(-4), pnorm(4), lwd = 2, xlab = "p", ylab = "Q(p)")
+
+
+#Funcion de distribucion emprica acumulada
+
+ggplot(data=mtcars, aes(x=mpg)) + 
+  stat_ecdf(geom = "step")+
+  labs(x = "MPG", y = "F(X)")+
+  theme(text = element_text(size=14))+
+  theme_grey(base_size = 16)
+
+  
+
+ggplot(data=mtcars, aes(x=hp)) + 
+  stat_ecdf(geom = "step")+
+  labs(x = "HP", y = "F(X)")+
+  theme(text = element_text(size=14))+
+  theme_grey(base_size = 16)
+
+
+
+#Grafica de cuantil -cuantil
+##Q-Q plot
+
+#En estadística, un gráfico de Q-Q (quantile-quantile) es un gráfico de probabilidad, que es un método gráfico para comparar dos distribuciones de probabilidad al trazar sus cuantiles uno contra el otro.En este caso, lo ideal es que los puntos se acerquen a una recta diagonal.
+
+ggplot(data = mtcars, mapping = aes(sample = mpg)) +
+  stat_qq_line()+
+  stat_qq_band()+
+  stat_qq_point()+
+  labs(x = "Q-Normal", y = "Q-MPG")+
+  theme(text = element_text(size=14))+
+  theme_grey(base_size = 16)
+
+
+#Cuantiles más frecuentes
+
+#Vamos a mostrar cuales son los cuantiles más utilizados en estadística. La mayoría de ellos son de uso habitual para poder analizar de forma detallada la distribución de los datos. Además, otra de sus utilidades es separar los datos en grupos, pudiendo elegir los más altos o los más bajos. En el ejemplo veremos esto con mayor detalle.
+#	Cuartil: Separa los valores en cuatro grupos iguales y existen tres cuartiles. Es el más frecuente. El cuartil uno (Q1) son los datos menores y el tres (Q3) los mayores. Por otro lado, el cuartil dos (Q2) se corresponde con la mediana (Me) que es un estadístico de posición que divide la distribución de los datos a la mitad. Los valores del cuantil serían 0.25 (Q1), 0.5(Q2) y 0.75 (Q3).
+#	Quintil: Similar al anterior, es menos frecuente y divide los datos en cinco partes iguales. Por tanto, hay cuatro quintiles. Los valores del cuantil en este caso serían 0.20, 0.40, 0.60, 0.80.
+#	Decil: En este caso se dividen en diez partes y, por tanto, hay nueve deciles. Una vez más, este tampoco es demasiado frecuente. Sus valores serían de 0.1 a 0.9.
+#	Percentiles: Estamos ante una variante en que la distribución se divide en cien partes iguales. Puede ser de interés para muestras muy numerosas. Sus valores van de 0.01 a 0.99.
+
+
+mtcars_stand <- apply(mtcars[,-c(8:9,12)], 2, scale)
+
+qq_mpg<-quantile(mtcars_stand[,1],probs = seq(0,1,0.01)) # calcula el percentil desde 0 a 100 con in incremento de 0.01
+
+
+qq_hp<-quantile(mtcars_stand[,4],probs = seq(0,1,0.01)) # calcula el percentil desde 0 a 100 con in incremento de 0.01
+
+
+
+
+#Generamos el dataframe de los dos percentiles
+data_qq<-data.frame(qq_mpg,qq_hp)
+
+
+#Graficamos nuevamente par ver mejor la distribución
+
+#png("qqplot.png",width = 4, height =4,units = "in",pointsize = 16,res = 300)
+
+ggplot(data_qq,aes(x=qq_mpg,y=qq_hp))+
+  geom_point()+
+  geom_abline(intercept=0, slope=1)+
+  labs(x = "Q-MPG", y = "Q-HP")+
+  theme(text = element_text(size=14))+
+  theme_grey(base_size = 16)
+
+
 
 
 
