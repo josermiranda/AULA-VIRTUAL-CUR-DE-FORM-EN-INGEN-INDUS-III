df5bb7bb0775e3d5cec1e426f5ece69cd6cdbd00
josermiranda <josermirandah@gmail.com>
2022-11-12 04:58:12 +0000

    Verificar media_moda_y mediana

---
+++ clase_11_11_2022.R
@@ -0,0 +1,22 @@
+mtcars$mpg
+
+hist(mtcars$mpg, breaks="Sturges")
+
+#importante#
+#cuando la media aritmetica es mayor que la mediana  la curva tiene sesgo hacia la derecha
+#cuando la mediana es mayor a la media aritmetica la cuerva tine sesgo a la izquierda #
+
+
+
+skewness(mtcars$mpg)
+# si el valor de skewness  es 0 indica que la curva es normal perfecta, es decir la moda= media= mediana#
+#si el valor es mayor a cero significa que la cuerva esta sesgada o es asimetrica  hacia la derecha#
+#si el valor es menor a cero significa que  la cuerva esta sesgada o es asimetrica  hacia la izquierda#
+
+hist(mtcars$hp, breaks="Sturges")
+
+#  la moda , la mediana y la media  es donde esta la mayor cantidad de datos concentradosm o en el intervalo de frecuencioa es la barra que muestra el mayor frecuencia absoluta#  
+
+skewness(mtcars$hp)
+
+
