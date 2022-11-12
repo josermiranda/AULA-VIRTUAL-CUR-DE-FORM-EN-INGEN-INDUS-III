2b18c9ce659b442533699d856e4a7982ef74ef67
josermiranda <josermirandah@gmail.com>
2022-11-12 03:39:48 +0000

    carga_2

--- clase_11_11_2022.R
+++ clase_11_11_2022.R
@@ -2,3 +2,14 @@
 install.packages(c("tidyverse", "visdat", "dlookr", "funModeling", "flextable", "inspectdf", "qqplotr", "ggpmisc", "PerformanceAnalytics", "corrplot"))
 
 
+
+library(tidyverse)
+library(visdat)
+library(dlookr)
+library(funModeling)
+library(flextable) 
+library(inspectdf) 
+library(qqplotr) 
+library(ggpmisc)
+library(PerformanceAnalytics)
+library(corrplot)
